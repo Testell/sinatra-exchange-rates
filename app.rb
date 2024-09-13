2287c7d15467200cf23193fae552e8b9070cf710
Tyler Estell <108036777+Testell@users.noreply.github.com>
2024-09-13 03:12:11 +0000

    grade error

--- app.rb
+++ app.rb
@@ -1,9 +1,51 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  @exchange_key = ENV.fetch("EXCHANGE_RATE_KEY")
+
+  @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=#{@exchange_key}")
+
+  @string_response = @raw_response.to_s
+
+  @parsed_response = JSON.parse(@string_response)
+
+  @currencies = @parsed_response.fetch("currencies")
+
+  erb(:homepage)
+end
+
+get("/:first_currency") do
+  @exchange_key = ENV.fetch("EXCHANGE_RATE_KEY")
+
+  @the_symbol = params.fetch("first_currency")
+
+  @raw_response = HTTP.get("https://api.exchangerate.host/list?access_key=#{@exchange_key}")
+
+  @string_response = @raw_response.to_s
+
+  @parsed_response = JSON.parse(@string_response)
+
+  @currencies = @parsed_response.fetch("currencies")
+
+  erb(:first_currency)
+end
+
+get("/:first_currency/:second_currency") do
+  @exchange_key = ENV.fetch("EXCHANGE_RATE_KEY")
+
+  @first = params.fetch("first_currency")
+  @second = params.fetch("second_currency")
+
+  # Construct the URL string
+  @url = "https://api.exchangerate.host/convert?from=#{@first}&to=#{@second}&amount=1&access_key=#{@exchange_key}"
+  
+  @raw_response = HTTP.get(@url)
+  @string_response = @raw_response.to_s
+  @parsed_response = JSON.parse(@string_response)
+
+  @amount = @parsed_response.fetch("result")
+  
+  erb(:second_currency)
 end
